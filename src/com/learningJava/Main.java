adb7fdc16ea5704e62aa76486846b1510705bc40
Yaro <yaro.muzychenko@gmail.com>
2020-03-25 09:37:21 -0600

    New

---
+++ src/com/learningJava/Main.java
@@ -0,0 +1,17 @@
+package com.learningJava;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int myValue = 10000;
+
+
+    int myMinInValue = Integer.MIN_VALUE;
+    int myMacInValue = Integer.MAX_VALUE;
+
+        System.out.println("Integere Min Value = " + myMinInValue);
+        System.out.println("Integere Max Value = " + myMacInValue);
+        System.out.println("Basted Max Value = " + (myMacInValue +1));
+        System.out.println("Basted Min Value = " + (myMinInValue - 1));
+    }
+}
